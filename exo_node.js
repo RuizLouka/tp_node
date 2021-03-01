9e082bdb7c3c4be01d291af8184bc5821355d504
RuizLouka <39903481+RuizLouka@users.noreply.github.com>
2021-03-01 22:26:31 +0100

    Create exo_node.js

---
+++ exo_node.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const map = require('through2-map')
+
+const server = http.createServer(function (req, res) {
+  if (req.method == 'POST') {
+   req.pipe(map((chunk) => {
+    return chunk.toString().toUpperCase()
+  }
+  )
+  ).pipe(res);
+  	}
+    )
+    ).pipe(res)
+    
+  } else {
+  	return res.end('It\'s not a POST method.')
+  }
+
+ 
+})
+
+server.listen(parseInt(process.argv[2]))
